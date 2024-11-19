48e0b77ac62053c6ce5672f8e9236b09710bae4a
Ana Germoglio <anasgermoglio@gmail.com>
2024-11-19 20:21:23 +0000

    4 - springboot newyear working

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,36 @@
 package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.DateTimeException;
+
+
 @SpringBootApplication
+@RestController
+
 public class Main {
-    public static void main(String[] args) {
+
+    @GetMapping("/newyear")
+    public String daysUntilNewYear(Model model) {
+        System.out.println("Calculating days to New Year..."); //Debug line
+        DateTime now = new DateTime();
+        DateTime newYear = new DateTime(now.getYear() + 1, 1, 1, 0, 0);
+        int daysUntilNewYear = Days.daysBetween(now, newYear).getDays();
+        System.out.println("Days remaining: + daysUntilNewYear");
+        model.addAttribute("daysUntilNewYear", daysUntilNewYear);
+        return "Days remaining: " + daysUntilNewYear;
+
+    }
+
+    public static void main(String[] args){
         SpringApplication.run(Main.class, args);
     }
+
 }
